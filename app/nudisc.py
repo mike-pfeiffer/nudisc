15bbc36c34c1621187f64e7cc2a2653b0dfd8f9f
Mike Pfeiffer <pfeiffermj@outlook.com>
2021-03-29 02:05:01 +0000

    adds rough-in support for tcp, udp port scans

--- app/nudisc.py
+++ app/nudisc.py
@@ -23,30 +23,22 @@ import xmltodict
 import subprocess
 
 
-def find_open_protocols():
+def find_open_protocols(ip_range, ip_protocols):
     # change this if looking for a different status
     protocol_status = 'open'
-    # icmp, udp, tcp
-    ip_protocols = '1,6,17'
-    ip_range = '192.168.0.0/16'
     # nmap results as xml file
-    output_filename = 'discovery/ipprotosweep.xml'
+    output_filename = 'discovery/ipprotosweep'
     # nmap command flags invoked by subprocess
     nmap_proto_scan = [
         'nmap', '-sO', '-p', ip_protocols, ip_range, '--min-hostgroup', '128',
-        '--min-rate', '4096', '-oX', output_filename
+        '--min-rate', '4096', '-oA', output_filename
     ]
     # use host to run nmap process, write output to null
     subprocess.call(nmap_proto_scan, stdout=open(os.devnull, 'wb'))
 
-    # TODO make sure this handles file exceptions properly
-    with open(output_filename) as xml_file:
+    with open(output_filename + ".xml") as xml_file:
         data_dict = xmltodict.parse(xml_file.read())
-        xml_file.close()
-
-    # TODO should I add logic to exit if data dict is empty?
-    if(data_dict):
-        print("yo")
+    xml_file.close()
 
     json_data = json.dumps(data_dict)
     parsed = json.loads(json_data)
@@ -86,12 +78,46 @@ def find_open_protocols():
                 else:
                     targets_dict[protocol_name].append(ipv4_addr)
 
+    confirmed_protocols = []
+
     for key, value in targets_dict.items():
-        with open("./targets/" + key + "_targets.txt", "w") as target_file:
+        filename = "targets/" + key + "_targets.txt"
+        if key == "tcp" or key == "udp":
+            confirmed_protocols.append(filename)
+        with open(filename, "w") as target_file:
             for ip in value:
                 target_file.write(ip + "\n")
-            target_file.close()
+        target_file.close()
+
+    return confirmed_protocols
+
+
+def find_open_ports(hit_list, tcp_ports, udp_ports):
+
+    for target in hit_list:
+        if "tcp" in target:
+            nmap_port_scan = [
+                'nmap', '-sS', '-n', '-p', tcp_ports, '-iL', target, '-oA',
+                'discovery/tcp_scan_results'
+            ]
+            subprocess.call(nmap_port_scan, stdout=open(os.devnull, 'wb'))
+        elif "udp" in target:
+            nmap_port_scan = [
+                'nmap', '-sU', '-n', '-p', udp_ports, '-iL', target, '-oA',
+                'discovery/udp_scan_results'
+            ]
+            subprocess.call(nmap_port_scan, stdout=open(os.devnull, 'wb'))
 
 
 if __name__ == '__main__':
-    find_open_protocols()
+    with open("scan.json") as scan_file:
+        scan_dict = json.loads(scan_file.read())
+        scan_file.close()
+
+    ip_range = scan_dict["ipv4_range"]
+    ip_protocols = scan_dict["ip_protocols"]
+    tcp_ports = scan_dict["tcp_ports"]
+    udp_ports = scan_dict["udp_ports"]
+
+    confirmed_protocols = find_open_protocols(ip_range, ip_protocols)
+    find_open_ports(confirmed_protocols, tcp_ports, udp_ports)
